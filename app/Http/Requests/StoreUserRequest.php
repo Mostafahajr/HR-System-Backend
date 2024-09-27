fa83844ac510b6efabb6d7a16f325c8e27ff92ab
Ahmed Atef <ahmed.atef.2019@outlook.com>
2024-09-27 10:55:16 +0300

    fixed validation for users

--- app/Http/Requests/StoreUserRequest.php
+++ app/Http/Requests/StoreUserRequest.php
@@ -22,11 +22,11 @@ class StoreUserRequest extends FormRequest
     public function rules(): array
     {
         return [
-            "name"=>['required'],
-            "username"=>['required'],
-            "email"=>['required'],
-            "password"=>['required'],
-            "group_type_id"=>['required']
+            "name" => ['required', 'min:3', 'max:40', 'regex:/^[A-Za-z ]{3,}$/'],
+            "username" => ['required', 'min:3', 'max:20', 'regex:/^[A-Za-z0-9 (.|*|&|^|%|@)]{3,}$/'],
+            "email" => ['required', 'email'],
+            'password' => ['regex:/^[A-Za-z0-9(@|#|$|%*)]{8,}$/'],
+            "group_type_id" => ['required']
         ];
     }
 }
